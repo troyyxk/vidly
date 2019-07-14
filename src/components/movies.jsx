62454c1fce7bb56c2db1ebf2e0ba6600578ff4ae
unknown <xingkunyin.hotmail.com>
2019-07-13 21:29:23 -0400

    start working on the like button

---
+++ src/components/movies.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+    // use setState to modify the state
+    // first one the old one and second one the new one
+    // if the new one and the one we wnat to replace the old one with have the same name, we can just use it and write onece
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+    // in jsx, render only return a single element
+    // if we want to return multiple elements, they should have a parent
+    return (
+      <div>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
