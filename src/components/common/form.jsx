a3ef1252a130cec4a6c282349f95b398d2d46375
unknown <xingkunyin.hotmail.com>
2019-07-21 03:22:44 -0400

    Finish all forms.

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
-  state = { data: {}, errors: {} };
+  state = {
+    data: {},
+    errors: {}
+  };
 
   validate = () => {
     const options = { abortEarly: false };
@@ -11,20 +15,11 @@ class Form extends Component {
     if (!error) return null;
 
     const errors = {};
-    for (let item of error.details) {
-      errors[item.path[0]] = item.message;
-    }
-
+    for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
 
   validateProperty = ({ name, value }) => {
-    // if (name === "username") {
-    //   if (value.trim() === "") return "Username is Required.";
-    // }
-    // if (name === "password") {
-    //   if (value.trim() === "") return "Password is Required.";
-    // }
     const obj = { [name]: value };
     const schema = { [name]: this.schema[name] };
     const { error } = Joi.validate(obj, schema);
@@ -33,10 +28,12 @@ class Form extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    const errors = this.validate();
 
+    const errors = this.validate();
     this.setState({ errors: errors || {} });
-    if (errors) return null;
+    if (errors) return;
+
+    this.doSubmit();
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -47,31 +44,47 @@ class Form extends Component {
 
     const data = { ...this.state.data };
     data[input.name] = input.value;
+
     this.setState({ data, errors });
   };
 
-  renderButton = label => {
+  renderButton(label) {
     return (
       <button disabled={this.validate()} className="btn btn-primary">
         {label}
       </button>
     );
-  };
+  }
 
-  renderInput = (name, label, type = "text") => {
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderInput(name, label, type = "text") {
     const { data, errors } = this.state;
 
     return (
       <Input
         type={type}
         name={name}
-        label={label}
         value={data[name]}
+        label={label}
         onChange={this.handleChange}
         error={errors[name]}
       />
     );
-  };
+  }
 }
 
 export default Form;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MoviesTable from "./moviesTable";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
-import MoviesTable from "./moviesTable";
 import _ from "lodash";
+import SearchBox from "./searchBox";
 
 class Movies extends Component {
   state = {
-    // movies: getMovies(),
-    // genres: getGenres(),
     movies: [],
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn: { path: "title", order: "asc" }
-    // selectedGenre: null
   };
 
   componentDidMount() {
     const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres: genres });
+
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies: movies });
-    // use setState to modify the state
-    // first one the old one and second one the new one
-    // if the new one and the one we wnat to replace the old one with have the same name, we can just use it and write onece
+    this.setState({ movies });
+
+    deleteMovie(movie._id);
   };
 
   handleLike = movie => {
-    // console.log(movie);
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
     movies[index] = { ...movies[index] };
@@ -46,28 +46,34 @@ class Movies extends Component {
   };
 
   handleGenreSelect = genre => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
-    // console.log("this.state.selectedGenre", this.state.selectedGenre);
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
+  };
+
+  handleSearch = query => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
   };
 
   handleSort = sortColumn => {
     this.setState({ sortColumn });
-    // console.log(path);
   };
 
-  getPageData = () => {
+  getPagedData = () => {
     const {
       pageSize,
       currentPage,
+      sortColumn,
       selectedGenre,
-      movies: allMovies,
-      sortColumn
+      searchQuery,
+      movies: allMovies
     } = this.state;
-    // take the correct movies
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
-        : allMovies;
+
+    let filtered = allMovies;
+    if (searchQuery)
+      filtered = allMovies.filter(m =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter(m => m.genre._id === selectedGenre._id);
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -78,29 +84,31 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const { totalCount, data: movies } = this.getPageData();
+    const { totalCount, data: movies } = this.getPagedData();
 
-    // console.log(movies);
-    // in jsx, render only return a single element
-    // if we want to return multiple elements, they should have a parent
     return (
       <div className="row">
-        {/* <div className="col-3"> */}
         <div className="col-3">
           <ListGroup
             items={this.state.genres}
             selectedItem={this.state.selectedGenre}
-            // textProperty="name"
-            // valueProperty="_id"
             onItemSelect={this.handleGenreSelect}
           />
         </div>
-
         <div className="col">
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
           <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
